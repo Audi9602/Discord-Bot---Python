67261947ccd8df64bdae97bff5f92e566c137725
Audi9602 <audisha9602@gmail.com>
2026-04-29 11:37:59 +0530

    Initial clean commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=intents)
+
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+client = MyClient()
+
+token = os.getenv("DISCORD_TOKEN")
+client.run(token)
